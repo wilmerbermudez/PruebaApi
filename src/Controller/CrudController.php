67e785ce1153d7f25973342fc187df62ed8c67be
Wilmer Bermudez <wfbermudez@utp.edu.co>
2022-09-08 16:44:43 -0500

    mostar productos desde api

---
+++ src/Controller/CrudController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CrudController extends AbstractController
+{
+    /**
+     * @Route("/crud", name="app_crud")
+     */
+    public function index(): Response
+    {
+        return $this->render('crud/index.html.twig');
+    }
+}
